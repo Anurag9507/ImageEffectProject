d1d951eb9d496d0144d50ab17f46f404b03f54d5
Anurag <Anurag.Ramaswamy@iiitb.ac.in>
2024-01-01 12:28:15 +0530

    Modified Invert

--- ImageEffectBackend/src/Libraries/InvertLibrary/Invert.cpp
+++ ImageEffectBackend/src/Libraries/InvertLibrary/Invert.cpp
@@ -1,26 +1,15 @@
 #include "Invert.h"
 #include "../Pixel.h"
 #include <vector>
-#include <algorithm>
-#include <iostream>
 using namespace std;
-
 // Function to invert the image (done by inversing the colors of RGB in each pixel)
-void applyInvert(vector<vector<Pixel>>& image) {
-    // Iterate through each row of the image
-    for (auto& row : image) {
-        // Iterate through each pixel in the current row
-        for (auto& pixel : row) {
-            // Invert each color channel in the pixel
-            pixel.r = 255 - pixel.r;  // Invert the red channel
-            pixel.g = 255 - pixel.g;  // Invert the green channel
-            pixel.b = 255 - pixel.b;  // Invert the blue channel
+void applyInvert(vector<vector<Pixel>>& image){
+    for(int i=0; i<image.size(); i++){// Iterate through each row of the image
+        for(int j=0;j<image[0].size();j++){// Iterate through each pixel in the current row
+        // Invert each color channel in the pixel
+            image[i][j].r = 255 - image[i][j].r;// Invert the red channel
+            image[i][j].g = 255 - image[i][j].g;// Invert the green channel
+            image[i][j].b = 255 - image[i][j].b;// Invert the blue channel
         }
     }
 }
-
-// Comments for the entire code file:
-// This file defines the function 'applyInvert', which inverts the colors of an image
-// represented as a 2D vector of Pixel objects. The inversion is achieved by subtracting
-// each color channel value from 255. The file includes necessary headers and uses the
-// 'std' namespace for convenience.
--- ImageEffectBackend/src/Libraries/InvertLibrary/Invert.h
+++ ImageEffectBackend/src/Libraries/InvertLibrary/Invert.h
@@ -5,7 +5,7 @@
 // Include necessary headers for the code
 #include <vector>
 #include "../Pixel.h"
-
+using namespace std;
 // Declaration of the function 'applyInvert'
 // This function takes a 2D vector representing an image, where each element is a Pixel object.
 // The function inverts the colors of the image by subtracting each color channel value from 255.
